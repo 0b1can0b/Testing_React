e712a496d7e9ba7f18e4d586aa5f2e18525d50b4
0b1can0b <0b1can0b21@gmail.com>
2022-12-09 01:19:46 +0530

    c

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,18 @@
+import { useEffect, useState } from "react";
 import "./App.scss";
 
+console.clear();
+
 const App = () => {
-  return <div className="app">App</div>;
+  return (
+    <div className="app">
+      Lorem ipsum dolor sit amet consectetur adipisicing elit. Ipsam nam labore
+      architecto voluptate, tempora minima, nemo temporibus ducimus, obcaecati
+      deserunt quasi aperiam exercitationem natus dolorem consequuntur dicta
+      unde quam modi sint sapiente. Impedit dignissimos mollitia praesentium
+      repellendus iste sint quis!
+    </div>
+  );
 };
 
 export default App;
--- src/main.jsx
+++ src/main.jsx
@@ -1,8 +1,12 @@
-import { StrictMode } from 'react';
-import ReactDOM from 'react-dom/client';
-import App from './App';
+import { StrictMode } from "react";
+import ReactDOM from "react-dom/client";
+import App from "./App";
 
-ReactDOM.createRoot(document.getElementById('root')).render(
+import "sanitize.css";
+import "sanitize.css/forms.css";
+import "sanitize.css/typography.css";
+
+ReactDOM.createRoot(document.getElementById("root")).render(
   <StrictMode>
     <App />
   </StrictMode>
