82eb5b5dec9d8a92b59ae1d012492cf216c82296
0b1can0b <0b1can0b21@gmail.com>
2022-12-10 18:32:57 +0530

    c

--- src/App.jsx
+++ src/App.jsx
@@ -3,14 +3,54 @@ import "./App.scss";
 
 console.clear();
 
+const navItems = ["Home", "About", "Services", "Portfolio", "FAQs", "Contact"];
+
 const App = () => {
+  const [IsHovered, setIsHovered] = useState(false);
+  const [hovered, setHovered] = useState({ left: 0, width: 0 });
+  const [active, setActive] = useState({ left: null, width: null });
+  const [clickEffect, setClickEffect] = useState(false);
+
+  const handelMouseEnter = (e) => {
+    setIsHovered(true);
+    setHovered({ left: e.target.offsetLeft, width: e.target.offsetWidth });
+  };
+  const handelClick = (e) => {
+    setActive({ left: e.target.offsetLeft, width: e.target.offsetWidth });
+    setClickEffect(true);
+    setTimeout(() => {
+      setClickEffect(false);
+    }, 500);
+  };
+  const handelMouseLeave = () => {
+    setIsHovered(false);
+  };
+
   return (
     <div className="app">
-      Lorem ipsum dolor sit amet consectetur adipisicing elit. Ipsam nam labore
-      architecto voluptate, tempora minima, nemo temporibus ducimus, obcaecati
-      deserunt quasi aperiam exercitationem natus dolorem consequuntur dicta
-      unde quam modi sint sapiente. Impedit dignissimos mollitia praesentium
-      repellendus iste sint quis!
+      <div className="nav">
+        <div
+          className={clickEffect ? "indicator clickEffect" : "indicator"}
+          style={{
+            left: (!IsHovered ? active.left : false) || hovered.left,
+            width: (!IsHovered ? active.width : false) || hovered.width,
+            opacity: active.left !== null && active.width !== null && 1,
+          }}
+        />
+        {navItems.map((e, i) => {
+          return (
+            <div
+              key={i}
+              className="item"
+              onMouseEnter={handelMouseEnter}
+              onClick={handelClick}
+              onMouseLeave={handelMouseLeave}
+            >
+              {e}
+            </div>
+          );
+        })}
+      </div>
     </div>
   );
 };
